213be992b30be2110d7a1aef3d4c1a9f5449376e
cmlp <321231035@qq.com>
2022-10-31 00:09:05 +0800

    添加信息

--- src/main/java/com/ynu/edu/mygithub/Entry.java
+++ src/main/java/com/ynu/edu/mygithub/Entry.java
@@ -4,5 +4,6 @@ public class Entry {
     public static void main(String[] args){
         System.out.println("github测试");
         System.out.println("我是吴道芬");
+        System.out.println("我们的包错啦！！！！！！！！！许祝愿");
     }
 }
