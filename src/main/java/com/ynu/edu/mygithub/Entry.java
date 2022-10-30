0d16e8c016a40ff5a3de4636bd1459a136366a3b
wudaofen <wudaofen@mail.ynu.edu.cn>
2022-10-31 00:06:22 +0800

    github开发

---
+++ src/main/java/com/ynu/edu/mygithub/Entry.java
@@ -0,0 +1,8 @@
+package com.ynu.edu.mygithub;
+
+public class Entry {
+    public static void main(String[] args){
+        System.out.println("github测试");
+        System.out.println("我是吴道芬");
+    }
+}
